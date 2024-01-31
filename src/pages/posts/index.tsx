62a8c2edd5da86a7970f270f5dd9a5398969e6d5
hyeonKii <khk5676@gmail.com>
2024-01-31 17:12:44 +0900

    feat: 게시글, 글쓰기 컴포넌트 작업 완료

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,13 @@
+import Header from "components/Header";
+import PostList from "components/PostList";
+import Footer from "components/Footer";
+
+export default function PostsPage() {
+    return (
+        <>
+            <Header />
+            <PostList hasNavigation={false} />
+            <Footer />
+        </>
+    );
+}
