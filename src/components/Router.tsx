2a48aa55b581a6d9a4b7aefcf85b9cd27cfc1e81
hyeonKii <khk5676@gmail.com>
2024-02-01 14:24:53 +0900

    feat: onChange 구현1

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -5,26 +5,37 @@ import PostDetail from "pages/posts/detail";
 import PostNew from "pages/posts/new";
 import PostEdit from "pages/posts/edit";
 import ProfilePage from "pages/profile";
-import { LoginPage } from "pages/login";
+import {LoginPage} from "pages/login";
 import SignUpPage from "pages/signup";
 
+interface RouterProps {
+    isAuth: boolean;
+}
 
-export default function Router() {
+export default function Router({isAuth}: RouterProps) {
+
     return (
         <>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/posts" element={<PostList />} />
-                <Route path="/posts/:id" element={<PostDetail />} />
-                <Route path="/posts/new" element={<PostNew />} />
-                <Route
-                    path="/posts/edit/:id"
-                    element={<PostEdit />}
-                />
-                <Route path="/profile" element={<ProfilePage />} />
-                <Route path="/login" element={<LoginPage />}/>
-                <Route path="/signup" element={<SignUpPage />}/>
-                <Route path="*" element={<Navigate replace to="/" />} />
+                {isAuth ? (
+                    <>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/posts" element={<PostList />} />
+                        <Route path="/posts/:id" element={<PostDetail />} />
+                        <Route path="/posts/new" element={<PostNew />} />
+                        <Route path="/posts/edit/:id" element={<PostEdit />} />
+                        <Route path="/profile" element={<ProfilePage />} />
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/signup" element={<SignUpPage />} />
+                        <Route path="*" element={<Navigate replace to="/" />} />
+                    </>
+                ) : (
+                    <>
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/signup" element={<SignUpPage />} />
+                        <Route path="*" element={<LoginPage />} />
+                    </>
+                )}
             </Routes>
         </>
     );
